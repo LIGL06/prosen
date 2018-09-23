89fff19cd70fbbc56e5b7dcfaab58962f1fbaec0
Ivan García <ivan@chefsurf.io>
2018-09-23 10:46:09 -0500

    Correo Test

--- form.php
+++ form.php
@@ -4,27 +4,31 @@ $email = isset($_POST['email']) ? $_POST['email'] : null;
 if($email!=null){
     $to      = $email;
     $subject = 'Gracias por ponerte en contacto';
-    $message = '
+    $message = "
 <html>
 <head>
   <title>Birthday Reminders for August</title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
+  <meta charset='utf-8'>
+  <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
+  <meta http-equiv='x-ua-compatible' content='ie=edge'>
 </head>
 <body>
-  <p>Here are the birthdays upcoming in August!</p>
-  <table>
-    <tr>
-      <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
-    </tr>
-    <tr>
-      <td>Johny</td><td>10th</td><td>August</td><td>1970</td>
-    </tr>
-    <tr>
-      <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
-    </tr>
-  </table>
+  <section>
+    <div class='container'>
+      <div class='row' style='padding-top:100px'>
+      <div class='card text-center col'>
+        <img class='card-img-top' src='http://res.cloudinary.com/hammock-software/image/upload/v1535418201/Untitled-1_gtggiz.png' alt='Card image cap'>
+          <div class='card-body'>
+            <p class='card-text'>Gracias por querer estar en contacto con nosotros. <br> Por favor revisa tu bandeja de correo <b>no deseado</b>. </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
 </body>
 </html>
-';
+";
 
     $headers[] = 'MIME-Version: 1.0';
     $headers[] = 'Content-type: text/html; charset=iso-8859-1';
@@ -34,6 +38,7 @@ if($email!=null){
     $headers[] = 'Bcc: PROSEN <contacto@prosen.com.mx>';
     $headers[] = 'From: Contacto PROSEN <contacto@prosen.com.mx>';
     
+    // print_r($message);
     // Mail it
     mail($to, $subject, $message, implode("\r\n", $headers));
 }
