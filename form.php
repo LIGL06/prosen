342e31a4eaab763474dea4f95f7707a573db6141
Iván García <luis.garcialuna@outlook.com>
2018-10-01 21:10:51 -0500

    mailing

--- form.php
+++ form.php
@@ -1,13 +1,14 @@
 <?php
 
 $email = isset($_POST['email']) ? $_POST['email'] : null;
-if($email!=null){
+$name = isset($_POST['name']) ? $_POST['name'] : null;
+if($email!=null && $name!=null){
     $to      = $email;
     $subject = 'Gracias por ponerte en contacto';
     $message = "
 <html>
 <head>
-  <title>Birthday Reminders for August</title>
+  <title>Email de contacto PROSEN</title>
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css'>
   <meta charset='utf-8'>
   <meta name='viewport' content='width=device-width, initial-scale=1, shrink-to-fit=no'>
@@ -20,6 +21,7 @@ if($email!=null){
       <div class='card text-center col'>
         <img class='card-img-top' src='http://res.cloudinary.com/hammock-software/image/upload/v1535418201/Untitled-1_gtggiz.png' alt='Card image cap'>
           <div class='card-body'>
+            <h5 class='text-center'>Hola, $name</h5>
             <p class='card-text'>Gracias por querer estar en contacto con nosotros. <br> Nostros responderemos pronto, revisa tu bandeja de correo <b>no deseado</b>. </p>
           </div>
         </div>
@@ -34,12 +36,14 @@ if($email!=null){
     $headers[] = 'Content-type: text/html; charset=iso-8859-1';
     
     // Additional headers
-    $headers[] = 'To: Usuario <'. $email .'>';
-    $headers[] = 'Bcc: PROSEN <contacto@prosen.com.mx>';
-    $headers[] = 'From: Contacto PROSEN <contacto@prosen.com.mx>';
+    $headers[] = "To: $name <$email>";
+    $headers[] = 'Bcc: Julio Morales <jmorales@prosen.com.mx>';
+    $headers[] = 'From: Servicio al cliente PROSEN <sac@prosen.com.mx>';
     
     // print_r($message);
     // Mail it
     mail($to, $subject, $message, implode("\r\n", $headers));
+}else{
+  print_r('error');
 }
 ?>
